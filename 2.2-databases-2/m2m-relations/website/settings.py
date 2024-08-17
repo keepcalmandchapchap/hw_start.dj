fd5f0a6320d4da22769f05aff986a623079dddae
Derevtsov Maksim <mdroad21@mail.ru>
2024-08-17 22:34:42 +0300

    delete pass

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_m2m_relations',
         'USER': 'postgres',
-        'PASSWORD': 'Soska819',
+        'PASSWORD': '...',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
